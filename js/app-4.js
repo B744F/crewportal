28174bc7f47b1a45e917c608c6a8fb55808bd24a
lomankam <201505.login@gmail.com>
2026-07-27 17:03:15 +0800

    Align departed status build metadata

--- js/app-4.js
+++ js/app-4.js
@@ -1,6 +1,6 @@
 (function(){
-  const VERSION = "8.2.10";
-  const BUILD = "20260727-1424";
+  const VERSION = "8.2.11";
+  const BUILD = "20260727-1654";
   const DEFAULT_FLIGHT_AIRLINE = "CI";
   const RAW_BASE="https://raw.githubusercontent.com/B744F/crewportal/main/data/";
   const FLIGHT_GATE_API="https://flightdeck-api.201505-login.workers.dev/api/flight-gate";
