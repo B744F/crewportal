4edffd93d7d005069267d117d064daeb3f068bd1
lomankam <201505.login@gmail.com>
2026-07-28 16:49:39 +0800

    Set parking browser polling to 30 seconds

--- js/app-4.js
+++ js/app-4.js
@@ -1,6 +1,6 @@
 (function(){
-  const VERSION = "8.2.22";
-  const BUILD = "20260728-1641";
+  const VERSION = "8.2.23";
+  const BUILD = "20260728-1649";
   const DEFAULT_FLIGHT_AIRLINE = "CI";
   const RAW_BASE="https://raw.githubusercontent.com/B744F/crewportal/main/data/";
   const FLIGHT_GATE_API="https://flightdeck-api.201505-login.workers.dev/api/flight-gate";
