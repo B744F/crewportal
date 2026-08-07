a2b795933e96e4b3e7459b7cab773f6bf4c87f68
lomankam <201505.login@gmail.com>
2026-08-07 13:34:51 +0800

    Set button icon padding to ten pixels

--- js/app-4.js
+++ js/app-4.js
@@ -1,6 +1,6 @@
 (function(){
-  const VERSION = "8.2.66";
-  const BUILD = "20260807-1235";
+  const VERSION = "8.2.67";
+  const BUILD = "20260807-1333";
   const DEFAULT_FLIGHT_AIRLINE = "CI";
   const RAW_BASE="https://raw.githubusercontent.com/B744F/crewportal/main/data/";
   const FLIGHT_GATE_API="https://flightdeck-api.201505-login.workers.dev/api/flight-gate";
