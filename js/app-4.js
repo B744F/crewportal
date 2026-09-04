560e92da53f5e47f2b4905d103a03e571e39daf5
lomankam <201505.login@gmail.com>
2026-09-04 22:01:23 +0800

    Refresh portal cache build

--- js/app-4.js
+++ js/app-4.js
@@ -1,6 +1,6 @@
 (function(){
   const VERSION = "8.2.86";
-  const BUILD = "20260904-2156";
+  const BUILD = "20260904-2200";
   const DEFAULT_FLIGHT_AIRLINE = "CI";
   const RAW_BASE="https://raw.githubusercontent.com/B744F/crewportal/main/data/";
   const FLIGHT_GATE_API="https://flightdeck-api.201505-login.workers.dev/api/flight-gate";
